ec20185505eaa7877cd2d9053e765b7814cfcdf6
zacharyrb99 <zach.boudreaux999@gmail.com>
2021-07-21 23:19:29 -0400

    finished markov machine

--- makeText.js
+++ makeText.js
@@ -1,2 +1,43 @@
 /** Command-line tool to generate Markov text. */
+const fs = require('fs');
+const markov = require('./markov');
+const axios = require('axios');
 
+function generateText(text){
+    let mm = new markov.MarkovMachine(text);
+    console.log(mm.makeText());
+}
+
+function makeText(path){
+    fs.readFile(path, 'utf8', (err, data) => {
+        if(err){
+            console.log(`Couldn't read file at ${path}: ${error}`);
+            process.exit(1);
+        }else{
+            generateText(data);
+        }
+    })
+}
+
+async function makeURLText(url){
+    let resp;
+    try{
+        resp = await axios.get(url);
+    }catch(err){
+        console.log(`Cannot read text at ${url}: ${err}`);
+        process.exit(1);
+    }
+
+    generateText(resp.data)
+}
+
+let [type, path] = process.argv.slice(2);
+
+if(type === 'file'){
+    makeText(path);
+}else if(type === 'url'){
+    makeURLText(path);
+}else{
+    console.log(`I don't know what to do!`);
+    process.exit(1);
+}
--- markov.js
+++ markov.js
@@ -1,5 +1,5 @@
 /** Textual markov chain generator */
-
+const {stripHtml} = require('string-strip-html')
 
 class MarkovMachine {
 
@@ -18,12 +18,46 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
+    let chains = new Map();
+
+    for(let i = 0; i < this.words.length; i++){
+      let twoWords = this.words[i] + ' ' + this.words[i + 1];
+      let nextWord = this.words[i + 2] || null;
+
+      if(chains.has(twoWords)){
+        chains.get(twoWords).push(nextWord);
+      }else{
+        chains.set(twoWords, [nextWord]);
+      }
+    }
+
+    this.chains = chains;
   }
 
+  choice(array){
+    return array[Math.floor(Math.random() * array.length)];
+  }
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
     // TODO
+    let keys = Array.from(this.chains.keys());
+    let key = this.choice(keys);
+    let out = [];
+
+    while(out.length < numWords && key !== null){
+      let [word1, word2] = key.split(' ');
+      out.push(word1);
+      key = word2 + ' ' + this.choice(this.chains.get(key));
+    }
+
+    out = out.join(' ');
+    out = stripHtml(out).result
+    return out;
   }
 }
+
+module.exports = {
+  MarkovMachine
+};
